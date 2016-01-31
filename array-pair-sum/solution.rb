6019377bae8823a089f1a08950713d1a5d6b62a6
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-31 15:28:22 -0500

    format input for requirements and loop through array with pointers indexed from the outside, incrementing towards the middle

--- array-pair-sum/solution.rb
+++ array-pair-sum/solution.rb
@@ -1,2 +1,37 @@
 require 'pry'
 
+INPUT = [2,3,4,5,6,1,1]
+
+## Given an integer array, output all pairs that sum up to a specific value k
+
+## Change the value of k for different inputs
+
+k = 5
+
+input = INPUT
+
+## only output a pairing once, so removing duplicates
+input.uniq!
+
+## sort array from smallest to largest
+input.sort!
+
+## pointers reference an index of the modified input array.
+## beginning of array
+pointer1 = 0
+
+## end of array
+pointer2 = INPUT.length - 1
+
+
+while pointer1 != pointer2 do
+  sum = input[pointer1] + input[pointer2]
+  if sum == k
+    puts "(#{input[pointer1]},#{input[pointer2]})"
+    pointer1 += 1
+  elsif sum <= k
+    pointer1 += 1
+  else
+    pointer2 -= 1
+  end
+end
