a92f9f93b1728795231dbefe0ebe971dcd101a74
ryanmmhmm <magowan.ryan@gmail.com>
2016-01-31 16:38:39 -0500

    adding create matrix method to create M[x,y] as multidimensional array with values of random intergers from 0 to 100

--- matrix-region-sum/solution.rb
+++ matrix-region-sum/solution.rb
@@ -1,2 +1,26 @@
 ## Matrix region sum problem.  Key is to make this most efficient
 
+## sum of a rectangle of values in Matrix O, designated by the coordinates
+## A and D (being opposing corners of an interior sub rectangle of O)
+
+require 'matrix'
+
+## Define Input matrix
+
+def create_matrix(width, height)
+  m = Array.new(width) { Array.new(height) }
+  loop_through_matrix(m)
+end
+
+def loop_through_matrix(matrix)
+  matrix.map.each do |subset|
+    subset.map.each do |value|
+      value = apply_random_number
+    end
+  end
+end
+
+def apply_random_number
+  value = Random.new
+  value.rand(100)
+end
