93378c18bfaa2df4b6d8eb5e347f90f8e7b969e2
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-01 14:24:36 -0500

    solved by converting string to character arrays and calculating all possible permutations

--- all-permutations-of-a-string/solution.rb
+++ all-permutations-of-a-string/solution.rb
@@ -1,3 +1,22 @@
 ## Calculate all permutations of a given string
 
-str = "verypermute"
+str = "yes"
+
+def join_sets(set)
+  set.map!.each do |x|
+    x = x.join
+  end
+
+  set
+end
+
+def permutations(string)
+  if string.length == 1
+    return string
+  else
+    chars = string.split('')
+    perms = chars.permutation.to_a
+
+    perms = join_sets(perms)
+  end
+end
