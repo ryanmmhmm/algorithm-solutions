bd12ba74f072ee49bcf0519bafbcf78426df6789
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-02 12:28:14 -0500

    adding test case, init variables, failure case and loop

--- balancing-brackets/solution.rb
+++ balancing-brackets/solution.rb
@@ -5,12 +5,25 @@ def balanced?(str)
     return true
   elsif str.length % 2 != 0
     return false
+  elsif str[0] == ")"
+    return false
+  else
+    init(str)
   end
 
-  ary = str.split('')
+  while (@flag == nil) do
+    @flag = scan_right
+  end
 
+  @flag
 end
 
+def init(str)
+  @ary = str.split('')
+  @l = 0  # pointer for left bracket
+  @r = 0  # pointer for right bracket
+  @flag = nil
+end
 
 
 
@@ -23,6 +36,9 @@ str = ")(" # true  (even, not closed)
 puts "---#{str} is #{balanced?(str)}---"
 
 str = "))" # false  (even, not closed)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "(()(" # false  (odd = false)
 puts "---#{str} is #{balanced?(str)}---"
 
 str = "(()" # false  (odd = false)
