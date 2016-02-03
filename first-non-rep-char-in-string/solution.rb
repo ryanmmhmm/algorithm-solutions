689a6cef97aeece3ee3d58bc722c9f2be0e082f7
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-03 13:48:19 -0500

    checking for repeating characters by using a stack and compairing length of stack and first and last values

---
+++ first-non-rep-char-in-string/solution.rb
@@ -0,0 +1,34 @@
+
+## output the first non repepating character
+
+def repeating?(str)
+
+  return "none" if str.length == 0
+
+  ary = str.split('')
+  stack = [] #use a stack to compare values
+
+  str.each_char do |c|
+    stack << c
+    if stack.length == 2 && (stack.first != stack.last)
+      stack.pop
+      break
+    elsif stack.length > 2 && (stack.first != stack.last)
+      stack = [c]
+    end
+  end
+
+  if stack.empty?
+    "none"
+  else
+    return stack.first
+  end
+end
+
+## if stack is empty, no repeating characters
+## if stack has character, and is same, stack.shift
+## if stack has characters, and is different, stack = c.to_a
+## if stack has a character, and is different, break
+
+str = "ddddhhherrt"
+puts "#{repeating?(str)}"
