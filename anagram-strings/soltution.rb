d2e1dd4590e7a3b702cfc756e8d2892afe14fa09
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-03 15:05:15 -0500

    rearranging calls, adding return value for format_string, testing with smaller valid inputs

--- anagram-strings/soltution.rb
+++ anagram-strings/soltution.rb
@@ -4,13 +4,10 @@
 ## letters exactly once
 
 def anagram?(str1, str2)
-  return false if (str1.length == 0) || (str2.length == 0)
-  return false if (str1.length != str2.length)
-
   str1 = format_string(str1)
   str2 = format_string(str2)
-
-  str1
+  return false if (str1.length == 0) || (str2.length == 0)
+  return false if (str1.length != str2.length)
 
 end
 
@@ -18,8 +15,11 @@ def format_string(string)
   string.tr!(' ','')
   string.gsub(/[`~!@#$%^&*()_+-={}\|:;"'<,>.?]/,'')
   string.downcase!
+  string
 end
 
-str1 = "Eleven is two"
-str2 = "Twelve plus one"
-puts "#{str1} is #{anagram?(str1,str2)}"  # true
+# str1 = "Eleven plus two"
+# str2 = "Twelve plus one"
+str1 = "dog"
+str2 = "god"
+puts "#{str1} + #{str2} is #{anagram?(str1,str2)}"  # true
