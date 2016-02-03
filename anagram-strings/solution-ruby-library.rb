5258b4cd9a3e7d52c7a12d6d1543aba7de0b044e
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-03 14:21:34 -0500

    Anagram, not palindrome

--- anagram-strings/solution-ruby-library.rb
+++ anagram-strings/solution-ruby-library.rb
@@ -1,13 +1,13 @@
 
+## an anagram is a type of worldplay, the result of rearranging the letters
+## of a word or phrase to produce a new word, or phrase, using all the original
+## letters exactly once
 
-def anagram?(str)
+def anagram?(str1, str2)
   return false if str.length == 0
 
-  reverse = str.reverse
-
-  if str == reverse
-    true
-  else
-    false
-  end
 end
+
+str1 = "Eleven is two"  # true
+str2 = "Twelve plus one"
+puts "#{str} is #{anagram?(str)}"
