0d2c755a366732f3a2e78cc380d35a64d226094b
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-03 14:42:15 -0500

    adding length check for both strings

--- anagram-strings/soltution.rb
+++ anagram-strings/soltution.rb
@@ -5,6 +5,7 @@
 
 def anagram?(str1, str2)
   return false if (str1.length == 0) || (str2.length == 0)
+  return false if (str1.length != str2.length)
 
   str1 = format_string(str1)
   str2 = format_string(str2)
