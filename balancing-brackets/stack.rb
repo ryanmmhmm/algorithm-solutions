7e43c33a1c99d4dc7b1f1a86882d0caff0c6c7bd
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-03 12:41:06 -0500

    stack solution is most efficient, can be modified to check for types of brackets as well

---
+++ balancing-brackets/stack.rb
@@ -0,0 +1,53 @@
+
+def balanced?(str)
+  if str.length % 2 != 0
+    return false
+  end
+
+  ary = str.split('')
+  stack = []
+
+  ary.each do |c|
+    if c == "("
+      stack << c
+    elsif (c == ")") && (stack.last == "(")
+      stack << c
+      stack.pop(2)
+    else
+      stack << c
+      break
+    end
+  end
+
+  if stack.empty?
+    true
+  else
+    false
+  end
+end
+
+####  INPUT
+
+str = ""  # true  (even)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = ")(" # true  (even, not closed)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "))" # false  (even, not closed)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "(()(" # false  (odd = false)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "(()" # false  (odd = false)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "(())"  # true  (even, closed, true)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "((())"  # false  (odd = false)
+puts "---#{str} is #{balanced?(str)}---"
+
+str = "(()())"  # true  (even, closed, true)
+puts "---#{str} is #{balanced?(str)}---"
