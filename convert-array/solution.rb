3fe4b8aade2608736122b6e3a875f3945a983b47
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-04 11:39:25 -0500

    adding test case and some examples to help explain the question

--- convert-array/solution.rb
+++ convert-array/solution.rb
@@ -5,15 +5,41 @@
 ## using constant extra space.
 ##
 
+## Example indexes relative to N:
+## [0...N,N+1...2N,2N+1...3N]  or
+## [a..aN,b..bN,c..cN]
+## for [aaaa bbbb cccc] (N = 4)
+## 1 = [abc aaa bbb ccc]
+## 2 = [aabbcc aa bb cc]
+## 3 = [aaabbbccc a b c]
+## 4 = [aaaa bbbb cccc]
+
 ## Input is a string of length 3N (if true)
 ## Output is a string of length 3N that has been rearranged
 
 ## Rules:
-## str.length >= 3
-## str.length % 3 == 0
+## str.length >= 3  must be at least 3 chars long
+## str.length % 3 == 0  will always be divisible by 3
 
 def convert_array(str)
   return false if str.length < 3
   return false if str.length % 3 == 0
 
+  N = str.length / 3
+  ary = str.split('')
+
+  ## beginning index positions of char sets to translate
+  a = 0
+  b = 1 * N
+  c = 2 * N
+
+  ## i must always be >= 1
+  for i in (1..N)
+  end
+
+  ary
+
 end
+
+input = "aaabbbccc"
+puts "#{input} translates to #{convert_array(input)}"
