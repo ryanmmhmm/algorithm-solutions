0ce65beb40e1ceefa674a512254b2a3e34035b0e
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-06 17:57:29 -0500

    punctuation can be it's own word as well

--- reverse-words-in-string/test_solution.rb
+++ reverse-words-in-string/test_solution.rb
@@ -11,11 +11,13 @@ class TestSolution < MiniTest::Test
     assert_equal("solver! algorithm Hello", reverse_words(@input))
   end
 
-  # def test_that_transforms_for_input2
-  # end
+  def test_that_multiple_spaces_are_removed
+    assert_equal("solver! algorithm Hello", reverse_words(@input.tr(' ','  ')))
+  end
 
-  # def test_that_transforms_for_input3
-  # end
+  def test_that_punctuation_can_be_its_own_word
+    assert_equal("! solver algorithm Hello", reverse_words("Hello algorithm solver !"))
+  end
 
   # def test_that_transforms_for_input4
   # end
