c77fe75d13667d913e2521b4027a05d5eaeaef31
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-05 23:08:17 -0500

    adding test solution file

---
+++ all-permutations-of-a-string/test_solution.rb
@@ -0,0 +1,21 @@
+require './test_helper'
+require 'solution.rb'
+
+class TestSolution < MiniTest::Test
+
+  def setup
+  end
+
+  def test_that_transforms_for_input1
+  end
+
+  def test_that_transforms_for_input2
+  end
+
+  def test_that_transforms_for_input3
+  end
+
+  def test_that_transforms_for_input4
+  end
+
+end
