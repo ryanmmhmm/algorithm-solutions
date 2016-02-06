78a0bd3a97bf0c088af505067df3eb887151f92e
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-05 23:40:41 -0500

    refactoring solution to be used better with testing

--- array-pair-sum/solution.rb
+++ array-pair-sum/solution.rb
@@ -1,37 +1,36 @@
-require 'pry'
-
-INPUT = [2,3,4,5,6,1,1]
 
 ## Given an integer array, output all pairs that sum up to a specific value k
-
 ## Change the value of k for different inputs
-
 k = 5
+input = [2,3,4,5,6,1,1]
 
-input = INPUT
+def array_pair_sums(input, sum_value)
+  k = sum_value
 
-## only output a pairing once, so removing duplicates
-input.uniq!
+  ## only output a pairing once, so removing duplicates
+  input.uniq!
 
-## sort array from smallest to largest
-input.sort!
+  ## sort array from smallest to largest
+  input.sort!
 
-## pointers reference an index of the modified input array.
-## beginning of array
-pointer1 = 0
+  ## pointers reference an index of the modified input array.
+  ## beginning & end index of array
+  pointer1 = 0
+  pointer2 = input.length - 1
 
-## end of array
-pointer2 = INPUT.length - 1
+  output = []
 
-
-while pointer1 != pointer2 do
-  sum = input[pointer1] + input[pointer2]
-  if sum == k
-    puts "(#{input[pointer1]},#{input[pointer2]})"
-    pointer1 += 1
-  elsif sum <= k
-    pointer1 += 1
-  else
-    pointer2 -= 1
+  while pointer1 != pointer2 do
+    sum = input[pointer1] + input[pointer2]
+    if sum == k
+      output.push("(#{input[pointer1]},#{input[pointer2]})")
+      pointer1 += 1
+    elsif sum <= k
+      pointer1 += 1
+    else
+      pointer2 -= 1
+    end
   end
+
+  output
 end
--- array-pair-sum/test_solution.rb
+++ array-pair-sum/test_solution.rb
@@ -4,18 +4,12 @@ require 'solution.rb'
 class TestSolution < MiniTest::Test
 
   def setup
+    @input = [2,3,4,5,6,1,1]
+    @sum_value = 5
   end
 
   def test_that_transforms_for_input1
-  end
-
-  def test_that_transforms_for_input2
-  end
-
-  def test_that_transforms_for_input3
-  end
-
-  def test_that_transforms_for_input4
+    assert_equal(["(1,4)","(2,3)"],array_pair_sums(@input,@sum_value))
   end
 
 end
