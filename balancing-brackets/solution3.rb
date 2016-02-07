4fa83c392bf2e16896f8102f2ab0091c906f0670
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-07 13:54:19 -0500

    completing solution3 to show why it is not as effective as a STACK

--- balancing-brackets/solution3.rb
+++ balancing-brackets/solution3.rb
@@ -11,18 +11,46 @@ def balanced?(string)
       closures[c.to_sym] += 1
     elsif closingSet.include?(c)
       match = closingSet.index(c)
+      match = openingSet[match]
       closures[match.to_sym] -= 1
+      break if closures[match.to_sym] < 0
     end
-
-    break if ....
-    ## this is where this solution breaks down.
-    ## you would have to check each bracket type and determine
-    ## if it is < 0 in the closures hash.
-    ## The STACK is the methodology because it checks as it handles
-    ## each character without having to reverse lookup
-
-    ## NOTE: hashes are fast when referencing keys
-    ## they are not as useful when referencing values
   end
 
+  ## this is where this solution breaks down.
+  ## you would have to check each bracket type and determine
+  ## if it is < 0 in the closures hash.
+  ## The STACK is the methodology because it checks as it handles
+  ## each character without having to reverse lookup at the end
+
+  ## NOTE: hashes are fast when referencing keys
+  ## they are not as useful when referencing values
+
+  output = nil
+
+  closures.each do |key,value|
+    if value < 0
+      output = false
+      break
+    else
+      output = true
+    end
+  end
+
+  output
 end
+
+string = "()"
+puts "#{string} is #{balanced?(string)}"
+
+string = "())"
+puts "#{string} is #{balanced?(string)}"
+
+string = "()(("
+puts "#{string} is #{balanced?(string)}"
+
+string = "(()())"
+puts "#{string} is #{balanced?(string)}"
+
+string = ")(" ## this case requires a break command if closure < 0
+puts "#{string} is #{balanced?(string)}"
