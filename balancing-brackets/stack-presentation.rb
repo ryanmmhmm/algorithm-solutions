e8bc63dbf47e7709285568cf3bf3762b4a031882
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-07 14:13:59 -0500

    adding multi bracket type inputs for verification

--- balancing-brackets/stack-presentation.rb
+++ balancing-brackets/stack-presentation.rb
@@ -33,3 +33,9 @@ puts "#{string} is #{balanced?(string)}"
 
 string = ")(" ## this case requires a break command if closure < 0
 puts "#{string} is #{balanced?(string)}"
+
+string = "({}[])" ## this case requires a break command if closure < 0
+puts "#{string} is #{balanced?(string)}"
+
+string = "({[}])" ## this case requires a break command if closure < 0
+puts "#{string} is #{balanced?(string)}"
