61d72a720396bf64e96d620e5af9d1838807ae56
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-07 13:00:16 -0500

    simple counter solution for problem with only one bracket type

---
+++ balancing-brackets/solution1.rb
@@ -0,0 +1,39 @@
+## Counter method for determining single-type bracket closure
+
+def balanced?(string)
+
+  stack = []
+  closure = 0
+
+  string.each_char do |c|
+
+    if c == "("
+      closure += 1
+    elsif c == ")"
+      closure -= 1
+    end
+
+    break if closure < 0
+  end
+
+  if closure != 0
+    return false
+  else
+    true
+  end
+
+end
+
+string = "()"
+puts "#{string} is #{balanced?(string)}"
+
+string = "())"
+puts "#{string} is #{balanced?(string)}"
+
+string = "()(("
+puts "#{string} is #{balanced?(string)}"
+
+string = ")(" ## this case requires a break command if closure < 0
+puts "#{string} is #{balanced?(string)}"
+
+
