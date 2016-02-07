14667ce92328a5f9f52423c90635262ba16e8699
ryanmmhmm <magowan.ryan@gmail.com>
2016-02-07 14:08:19 -0500

    stack example is much more elegant than a hash table counter method, has better performance as well

---
+++ balancing-brackets/stack-presentation.rb
@@ -0,0 +1,35 @@
+## bracket closure stack example for presentation with multiple bracket types
+
+def balanced?(string)
+  set = {"(": ")", "{": "}", "[": "]"}
+  stack = []
+
+  string.each_char do |c|
+    if set.include?(c.to_sym)  # open bracket?
+      stack.push(c)
+    elsif set.values.include?(c) && stack.last == set.key(c).to_s  # closed bracket with closure?
+      stack.push(c)
+      stack.pop(2)
+    else  # closed bracket without closure
+      stack.push(c)
+      break
+    end
+  end
+
+  stack.empty?  ## stack will have values even when all open brackets
+end
+
+string = "()"
+puts "#{string} is #{balanced?(string)}"
+
+string = "())"
+puts "#{string} is #{balanced?(string)}"
+
+string = "()(("
+puts "#{string} is #{balanced?(string)}"
+
+string = "(()())"
+puts "#{string} is #{balanced?(string)}"
+
+string = ")(" ## this case requires a break command if closure < 0
+puts "#{string} is #{balanced?(string)}"
